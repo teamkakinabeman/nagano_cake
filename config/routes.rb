3f47329641360b335e0167ab8319621475c9b777
MasudaRintarou <112672796+rin13zou@users.noreply.github.com>
2022-10-18 13:15:00 +0000

    [Finish] Edit_customer_routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip:[:registrations, :passwords],controllers:{
     sessions: "admin/sessions"
   }
-  devise_for :customers
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
