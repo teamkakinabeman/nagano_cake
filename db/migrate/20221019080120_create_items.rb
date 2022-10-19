919fbbec1df0d75b56e0d791084f30c029b11f56
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-10-19 08:11:46 +0000

    [Add] itemのカラムを追加しmigrate

--- db/migrate/20221019080120_create_items.rb
+++ db/migrate/20221019080120_create_items.rb
@@ -1,7 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.1]
+  
   def change
     create_table :items do |t|
-
+      
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :unit_price, null: false
+      t.boolean :is_on_sale, null: false, default: true
+      
       t.timestamps
     end
   end
