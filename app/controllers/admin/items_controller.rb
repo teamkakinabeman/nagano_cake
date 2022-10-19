349e24825c0c434433b3f6183b847ac358b59c64
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-10-19 13:23:24 +0000

    [Fix] 管理者側itemsコントローラーの記述を修正2

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
 
   # ↓管理者以外はトップページにリダイレクトさせるコード
   before_action :if_not_admin
-  
+
   def index
     @items = Items.all
   end
@@ -10,25 +10,25 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
       flash[:notice] = "商品を登録しました。"
-      redirect_to adminitem_path(@item.id)
+      redirect_to admin_item_path(@item.id)
     else
       render 'index'
     end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -38,16 +38,16 @@ class Admin::ItemsController < ApplicationController
       render 'edit'
     end
   end
-  
-  
-  
+
+
+
   private
 
   # ↓管理者以外はトップページにリダイレクトさせるコード
   def if_not_admin
     redirect_to root_path unless current_user.admin?
   end
-  
+
   def item_params
     params.require(:item).permit(:name, :introduction, :unit_price, :is_on_sale)
   end
