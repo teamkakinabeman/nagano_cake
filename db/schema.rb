8a5965c872017f32ab7d6db19dc67aa2a6a36fcd
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-10-20 12:13:32 +0000

    [Add]itemsテーブルにgenre_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_20_113240) do
+ActiveRecord::Schema.define(version: 2022_10_20_121230) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2022_10_20_113240) do
     t.boolean "is_on_sale", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
