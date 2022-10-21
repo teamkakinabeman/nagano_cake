407ba63fa127bb9a929a68b023c0bdb2d72f0d6f
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-10-21 10:07:00 +0000

    [Add] 商品の販売ステータスにEnumを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
 
   has_one_attached :item_image
 
+  enum is_on_sale: { 販売中: true, 売切れ: false }
 
   def get_item_image
     (item_image.attached?) ? item_image : 'no_image.jpg'
