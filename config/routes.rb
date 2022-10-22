eec43b4ec5bf31ee02b2b603cf52236f3dabf107
moenakashima <111489156+moenakashima@users.noreply.github.com>
2022-10-22 20:26:15 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root :to => "homes#top"
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :items
-    resources :genres    
   end
   
   # 管理者用
