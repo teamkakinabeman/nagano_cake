2531436d1a9254fafe3331c7c6972a5c010cdf8b
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-10-24 02:50:00 +0000

    管理者ログインレイアウト編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_20_124421) do
+ActiveRecord::Schema.define(version: 2022_10_23_040718) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -103,18 +103,6 @@ ActiveRecord::Schema.define(version: 2022_10_20_124421) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "genre_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "postcode", null: false
-    t.string "address", null: false
-    t.string "name", null: false
-    t.integer "total_payment", null: false
-    t.integer "postage", null: false
-    t.integer "payment_method", default: 0, null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
