cb2d3e7ef10f961a638f8e8bb583fe3235ac27ec
sonoda03 <proyou2022@gmail.com>
2022-10-26 13:55:45 +0000

    [test-Fix]public/cart_itemsのプルダウン個数表記,ボタン表示位置調整

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,9 +3,6 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
 
-# カート内数量制限
-  validates :amount, ength: { maximum: 20 }
-
   ## 小計を求めるメソッド
   def subtotal
       item.add_tax_price*amount
