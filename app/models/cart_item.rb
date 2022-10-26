f3b41f60624c5a80d434b9c8868b7204a273afad
sonoda03 <proyou2022@gmail.com>
2022-10-26 12:58:53 +0000

    カート内バリテーション編集途中

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,11 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :item
   belongs_to :customer
-  
+
+# カート内数量制限
+  validates :amount, ength: { maximum: 20 }
+
   ## 小計を求めるメソッド
   def subtotal
       item.add_tax_price*amount
