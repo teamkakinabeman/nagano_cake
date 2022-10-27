3042bb366ec2a7b94525cc114ebbf49f471e366e
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-10-27 13:15:38 +0000

    [Fix]テストの問題点修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postcode,:address,:telephone_number])
   end
